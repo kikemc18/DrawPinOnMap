3db89e2270d0505f6a72f0c7c87c810a5d6c2978
kikemc18 <36907272+kikemc18@users.noreply.github.com>
2022-08-12 12:05:21 -0500

    1.0

---
+++ CreadorImagen.cs
@@ -0,0 +1,167 @@
+﻿using ImageMagick;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PinMapa
+{
+	public class CreadorImagen
+	{
+      
+        
+
+        public static byte[] ImagenFin( ) 
+        {
+            byte[] bytesArchivo;
+            try
+            {
+                string plantilla = @"Assets\Plantilla.jpg";
+                string pathPin = @"Assets\Pin.png";
+                string path1 = @"Assets\Rojo.png";
+
+                string pathFinal = Path.Combine(Path.GetTempPath(),"Pin.png");
+
+                using (MagickImage combiaciondePin = new MagickImage(plantilla))
+                {
+                    MagickImage pin = new MagickImage(pathPin);
+                    MagickImage p1 = new MagickImage(path1);
+                    MagickImage p2 = new MagickImage(path1);
+                    MagickImage p3 = new MagickImage(path1);
+                    MagickImage p4 = new MagickImage(path1);
+
+                    pin.Colorize(MagickColors.Pink, new Percentage(100)); // Cambiar de Color 
+                    p1.Colorize(MagickColors.Blue, new Percentage(100));
+                    p2.Colorize(MagickColors.Green, new Percentage(100));
+                    p3.Colorize(MagickColors.BlueViolet, new Percentage(100));
+                    p4.Colorize(MagickColors.Red, new Percentage(100));
+
+                    combiaciondePin.Scale(new Percentage(110), new Percentage(110)); // cambiar la escala
+                    p1.Scale(new Percentage(10), new Percentage(10));
+                    p2.Scale(new Percentage(10), new Percentage(10));
+                    p3.Scale(new Percentage(10), new Percentage(10));
+                    p4.Scale(new Percentage(10), new Percentage(10));
+
+                    combiaciondePin.Composite(pin, 350, 200); //Combinar Imagenes
+                    combiaciondePin.Composite(p1, 210, 80);
+                    combiaciondePin.Composite(p4, 410, 0);
+                    combiaciondePin.Composite(p2, 610, 0);
+                    combiaciondePin.Composite(p2, 810, 80);
+
+                    var img = new MagickImage(combiaciondePin);
+                    img.ColorFuzz = new Percentage(10);
+
+                    img.Transparent(MagickColors.White); //Quita el fondo Blanco
+                    img.Transparent(MagickColors.Black); // Quitra el fondo Negro
+                    //img.Transparent(MagickColors.Gray); //Quita el gris
+
+                   bytesArchivo = img.ToByteArray();
+                   
+
+
+                    img.Write(pathFinal); // Guardar
+                }
+
+            }
+            catch (Exception)
+            {
+                bytesArchivo = null;
+
+                throw;
+            }
+
+            return bytesArchivo;
+        }
+
+       // int a = 0;
+
+        public async void PinImagen(bool P1, bool P2, bool P3, bool P4 , int a) 
+        {      
+            try
+            {
+                string plantilla = @"Assets\Plantilla.jpg";
+                string pathPin = @"Assets\Pin.png";
+                string path1 = @"Assets\Rojo.png";
+                string pathFinal = Path.Combine(Path.GetTempPath(), "Pin" + a + ".png");
+
+                using (MagickImage combiaciondePin = new MagickImage(plantilla))
+                {
+                    MagickImage pin = new MagickImage(pathPin); //Uso de MagickImage para poder usar 
+                    MagickImage p1 = new MagickImage(path1);
+                    MagickImage p2 = new MagickImage(path1);
+                    MagickImage p3 = new MagickImage(path1);
+                    MagickImage p4 = new MagickImage(path1);
+
+                    pin.Colorize(MagickColors.Orange, new Percentage(100)); // Cambiar de Color 
+                    p1.Colorize(MagickColors.Blue, new Percentage(100));
+                    p2.Colorize(MagickColors.Green, new Percentage(100));
+                    p3.Colorize(MagickColors.BlueViolet, new Percentage(100));
+                    p4.Colorize(MagickColors.Red, new Percentage(100));               
+
+                    combiaciondePin.Scale(new Percentage(110), new Percentage(110)); // cambiar la escala
+                                                                                     // 
+                    p1.Scale(new Percentage(10), new Percentage(10));
+                    p2.Scale(new Percentage(10), new Percentage(10));
+                    p3.Scale(new Percentage(10), new Percentage(10));
+                    p4.Scale(new Percentage(10), new Percentage(10));
+
+                    combiaciondePin.Composite(pin, 350, 200);
+
+                    if (P1)
+                    {
+                        combiaciondePin.Composite(p1, 210, 80);
+                    }
+                    if (P2)
+                    {
+                        combiaciondePin.Composite(p2, 410, 0);
+                    }
+                    if (P3)
+                    {
+                        combiaciondePin.Composite(p3, 610, 0);
+                    }
+
+                    if (P4)
+                    {
+                        combiaciondePin.Composite(p4, 810, 80);
+                    }
+
+                    //combiaciondePin.Composite(pin, 350, 200); //Combinar Imagenes
+                    //combiaciondePin.Composite(p1, 210, 80);
+                    //combiaciondePin.Composite(p4, 410, 0);
+                    //combiaciondePin.Composite(p2, 610, 0);
+                   // combiaciondePin.Composite(p2, 810, 80);
+
+
+                   
+                    var img = new MagickImage(combiaciondePin);
+                    img.ColorFuzz = new Percentage(10);
+                    
+                    img.Transparent(MagickColors.White); //Quita el fondo Blanco
+                    img.Transparent(MagickColors.Black); // Quitra el fondo Negro
+                                                         //img.Transparent(MagickColors.Gray); //Quita el gris                
+                    a++;
+                    img.Write(pathFinal); // Guardar
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Controls.Maps;
+using System.Collections.ObjectModel;
+using Windows.Storage.Streams;
+using System.Drawing;
+using ImageMagick;
+using Image = System.Drawing.Image;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PinMapa
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MapElementsLayer myLayer1;
+        IList<MapLayer> myLayers = new ObservableCollection<MapLayer>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+           
+        }
+
+        private void myMap_Loaded(object sender, RoutedEventArgs e)
+        {         
+            myMap.Center = MainPage.SeattleGeopoint;
+            myMap.ZoomLevel = 12;      
+            AddLayer1();
+            
+            myMap.Layers = this.myLayers;          
+        }
+
+        private void AddLayer1()
+        {
+            this.myLayer1 = new MapElementsLayer();
+            this.myLayer1.MapElements = CreateCityBuildingsCollection();
+            this.myLayers.Add(this.myLayer1);         
+        }
+     
+        public static readonly Geopoint SeattleGeopoint = new Geopoint(new BasicGeoposition() { Latitude = 19.018567, Longitude = -98.265844 });
+        public static List<MapElement> CreateCityBuildingsCollection()
+        {
+            string Estado = "Disponible";
+            string ColorPin = "Rojo";
+            string ImageName = "";
+            bool FueraDeZona,FueraHorario,SinSenal, SinReporteUbicacion;
+
+            FueraDeZona = true;
+            FueraHorario = true;
+            SinSenal = false;
+            SinReporteUbicacion = false;
+
+            if (Estado == "Disponible") ColorPin = "Rojo";
+            else if (Estado == "Alerta Asignada") ColorPin = "Verde";
+
+            ImageName = ColorPin + "_";
+            ImageName += (FueraDeZona) ? "S" : "N";
+            ImageName += FueraHorario ? "S" : "N";
+            ImageName += (SinSenal) ? "S" : "N";
+            ImageName += (SinReporteUbicacion) ? "S" : "N";
+
+            RandomAccessStreamReference PinMap = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/"+ImageName+".png")); 
+
+            //RandomAccessStreamReference Pin2 = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/Azul.png")); 
+
+
+
+            //  //ms-appdata:///temp/Pin5.png
+            //  byte[] PIN;
+
+            //  PIN = CreadorImagen.ImagenFin();
+
+            //RandomAccessStreamReference Pin1 =RandomAccessStreamReference.CreateFromUri(new Uri("ms-appdata:///temp/Pin5.png"));
+
+
+
+            //  Stream memstr = new MemoryStream(PIN);
+
+
+
+
+            //  RandomAccessStreamReference Randon = RandomAccessStreamReference.CreateFromStream(memstr.AsRandomAccessStream());
+
+
+
+
+            /// RandomAccessStreamReference Pin = RandomAccessStreamReference.CreateFromStream(nose);
+
+
+
+            Geopoint ubicacion = new Geopoint(new BasicGeoposition() { Latitude = 19.018587, Longitude = -98.265848 });
+        
+            return new List<MapElement>
+            {
+                 new MapIcon { Location = ubicacion, NormalizedAnchorPoint = new Windows.Foundation.Point(0.5, 0.1), Image = PinMap,ZIndex = 0,Title="PIN"}
+            };
+            
+        }
+
+        private void findAtCenterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            CreadorImagen n = new CreadorImagen();
+
+            //for (int a=0; a < 100; a++) {
+              
+               n.PinImagen(true, false, false, false, 5);
+               n.PinImagen(true, true, false, false, 6);
+               n.PinImagen(true, true, true, false, 7);
+               n.PinImagen(true, true, true, true, 8);
+
+
+            //}
+        }
+    }
+}
